9d17d13a9de00c7dec56cca8b616a7fa35ad1025
shaankhank123001 <shaankhank42@gmail.com>
2024-11-29 02:25:16 +0500

    Update 0_AADI-CHICKEN.js

--- Priyansh/commands/0_AADI-CHICKEN.js
+++ Priyansh/commands/0_AADI-CHICKEN.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chicken")==0 || event.body.indexOf("Chicken")==0 || event.body.indexOf("CHICKEN")==0 || event.body.indexOf("CHIKAN")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑺𝑯𝑨𝑨𝑵 𝑲𝑯𝑨𝑵 𝑲 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n__________________________________ ",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/62306739ed186731b1f646029335e853.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
