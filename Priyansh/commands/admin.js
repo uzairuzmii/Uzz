d052a9fe8e5cdb573a6a46c335cadafc5ffb1158
shaankhank123001 <shaankhank42@gmail.com>
2024-11-29 02:15:17 +0500

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -84,7 +84,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
 
        
         case "add": { 
-            if (event.senderID != 100037743553265) return api.sendMessage(`𝗠𝗢𝗗𝗘 - 𝗕𝗼𝗿𝗱𝗲𝗿 𝗰𝗮𝗻𝗴𝗹𝗲 𝗿𝗶𝗴𝗵𝘁𝘀 🎀 `, event.threadID, event.messageID)
+            if (event.senderID != 100016828397863) return api.sendMessage(`𝗠𝗢𝗗𝗘 - 𝗕𝗼𝗿𝗱𝗲𝗿 𝗰𝗮𝗻𝗴𝗹𝗲 𝗿𝗶𝗴𝗵𝘁𝘀 🎀 `, event.threadID, event.messageID)
             if (permssion != 3) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
             if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
             if (mention.length != 0 && isNaN(content[0])) {
