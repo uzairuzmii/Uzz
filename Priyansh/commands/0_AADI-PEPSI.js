4f12ad6a81721afcd6324f4920a3ea54bec53015
Ayeahakhan <ayeshakhan831847@gmail.com>
2024-12-06 11:15:26 +0530

    Update 0_AADI-PEPSI.js

--- Priyansh/commands/0_AADI-PEPSI.js
+++ Priyansh/commands/0_AADI-PEPSI.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐊𝐇𝐀𝐍   𝐒𝐀𝐇𝐁 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐊𝐇𝐀𝐍  𝐒𝐀𝐇𝐀𝐁 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/17a1dba0466065978ecde714c2ddbb72.gif`)
                         }
                         api.sendMessage(msg, threadID, messageID);
@@ -23,4 +23,4 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         }
         module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-        }
+        
