2ad36c23fedee2929ad571a3d304ed9c288f3e78
Ayeahakhan <ayeshakhan831847@gmail.com>
2024-12-06 09:31:16 +0530

    Update 0_AADI-MOMOS.js

--- Priyansh/commands/0_AADI-MOMOS.js
+++ Priyansh/commands/0_AADI-MOMOS.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("momos")==0 || event.body.indexOf("Momos")==0 || event.body.indexOf("MOMOS")==0 || event.body.indexOf("MOMOSS")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐌𝐎𝐌𝐎𝐒 \n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐊𝐇𝐀𝐍   𝐒𝐀𝐇𝐁 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐌𝐎𝐌𝐎𝐒 \n__________________________________ ",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/9428f494ff4af77478041fb45a9f7b4f.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
