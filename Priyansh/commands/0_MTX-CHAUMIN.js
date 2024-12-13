90596c6ec4be56a6a45fe8486dc5009636b95bb0
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 18:23:54 +0500

    Update 0_MTX-CHAUMIN.js

--- Priyansh/commands/0_MTX-CHAUMIN.js
+++ Priyansh/commands/0_MTX-CHAUMIN.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("chaumin")==0 || event.body.indexOf("Chaumin")==0 || event.body.indexOf("CHAUMIN")==0 || event.body.indexOf("chauminn")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/8a9f84ed741b6df2854fb136dd028e79.jpg`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/images (1).jpeg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍜", event.messageID, (err) => {}, true)
