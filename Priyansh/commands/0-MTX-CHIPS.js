8863ba0448129d5bd0336d69c62d239f96a29a7e
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 21:54:49 +0500

    Update 0-MTX-CHIPS.js

--- Priyansh/commands/0-MTX-CHIPS.js
+++ Priyansh/commands/0-MTX-CHIPS.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("chips")==0 || event.body.indexOf("Chips")==0 || event.body.indexOf("CHIPS ")==0 || event.body.indexOf("Chipas")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-mini-aesthetics-snack-party.mp4`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/party.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥟", event.messageID, (err) => {}, true)
