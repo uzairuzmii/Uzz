77773a15583b43a42ed8ca916953a66d8ab0cbf1
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 19:56:58 +0500

    Update 0_MTX-DUDH.js

--- Priyansh/commands/0_MTX-DUDH.js
+++ Priyansh/commands/0_MTX-DUDH.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("dodh")==0 || event.body.indexOf("DOODH")==0 || event.body.indexOf("milk")==0 || event.body.indexOf("Milk")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n__________________________________",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/(720p).mp4`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/milk.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍼", event.messageID, (err) => {}, true)
