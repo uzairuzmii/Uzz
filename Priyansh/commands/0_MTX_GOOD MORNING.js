ecea599677f6e98923eba784d80c1b2769232567
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 18:13:11 +0500

    Create 0_MTX_GOOD MORNING.js

--- Priyansh/commands/0_MTX_GOOD MORNING.js
+++ Priyansh/commands/0_MTX_GOOD MORNING.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
 		var msg = {
 				body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕 \n__________________________________\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n__________________________________",
-				attachment: fs.createReadStream(__dirname + `/mtxuzair/received_1083155713406602.mp4`)
+				attachment: fs.createReadStream(__dirname + `/mtxuzair/images.jpeg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
