35b17ad930e7827dc88be4a209da41b41fb9f316
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 20:42:18 +0500

    Update 0_MTX-GULABJAMUN.js

--- Priyansh/commands/0_MTX-GULABJAMUN.js
+++ Priyansh/commands/0_MTX-GULABJAMUN.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("julabjamun")==0 || event.body.indexOf("GULABJAMUN")==0 || event.body.indexOf("Gulabjamun")==0 || event.body.indexOf("sargulla")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐆𝐮𝐥𝐚𝐁𝐣𝐚𝐌𝐮𝐧 \n__________________________________",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-Foodie-Heaven-Dessert-Recipe-Cake-Recipe-Cooking-Tips-Gulab-Jamun-Recipe.mp4`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/images.jpeg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
