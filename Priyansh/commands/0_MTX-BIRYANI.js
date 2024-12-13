335bbf5fe69c57c250ca70af2bec9f5a4d3e3e28
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 21:52:56 +0500

    Create 0_MTX-BIRYANI.js

--- Priyansh/commands/0_MTX-BIRYANI.js
+++ Priyansh/commands/0_MTX-BIRYANI.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 module.exports.config = {
-        name: "momos",
+        name: "biryani",
     version: "1.0.1",
         hasPermssion: 0,
         credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
-        usages: "momos",
+        usages: "biryani",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
-        if (event.body.indexOf("momos")==0 || event.body.indexOf("Momos")==0 || event.body.indexOf("MOMOS")==0 || event.body.indexOf("MOMOSS")==0) {
+        if (event.body.indexOf("biryani")==0 || event.body.indexOf("Biryani")==0 || event.body.indexOf("BIRYANI")==0 || event.body.indexOf("BIRYANI")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐌𝐎𝐌𝐎𝐒 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/9428f494ff4af77478041fb45a9f7b4f.jpg`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/biryani.jpeg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😍", event.messageID, (err) => {}, true)
