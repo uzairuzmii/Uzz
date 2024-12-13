d097ba2ee5db0fc700483a3ba2afd98e6287a761
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 22:16:16 +0500

    Update 0_MTX_GOLGAPPE.js

--- Priyansh/commands/0_MTX_GOLGAPPE.js
+++ Priyansh/commands/0_MTX_GOLGAPPE.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
 				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕 === 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
-				attachment: fs.createReadStream(__dirname + `/mtxuzair/Pin_on_Pins_by_you(360P)(1).mp4`)
+				attachment: fs.createReadStream(__dirname + `/mtxuzair/golmol.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
