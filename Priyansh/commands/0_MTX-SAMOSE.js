4fba19b88e75bc8764920d6903b128a1b4230cb0
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 18:22:33 +0500

    Update 0_MTX-SAMOSE.js

--- Priyansh/commands/0_MTX-SAMOSE.js
+++ Priyansh/commands/0_MTX-SAMOSE.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("Samose")==0 || event.body.indexOf("samosa")==0 || event.body.indexOf("SAMOSE")==0 || event.body.indexOf("SAMOSA")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐒𝐚𝐦𝐨𝐒𝐞\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/samosas_lover_here_|_Ricette,_Easy,_Crockpot(360P).mp4`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/images (2).jpeg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
