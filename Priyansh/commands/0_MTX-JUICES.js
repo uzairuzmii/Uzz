58bee73aa0503b6d843496577a4f3203e4c71a3e
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 21:18:49 +0500

    Update 0_MTX-JUICES.js

--- Priyansh/commands/0_MTX-JUICES.js
+++ Priyansh/commands/0_MTX-JUICES.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("juice")==0 || event.body.indexOf("juices")==0 || event.body.indexOf("Juice")==0 || event.body.indexOf("JUICE")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕   \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Never_forget_to_hydrate_your_body_every_day🥤🍓😋_|_Gıda,_Smoothies,_Çilekler(480P).mp4`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/juice.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍺", event.messageID, (err) => {}, true)
