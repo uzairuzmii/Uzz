83f46f414da9cd020e0c135acfe907136d4bfd46
uzairuzmii <uzairrajputking@gmail.com>
2024-12-13 22:02:56 +0500

    Update 0_MTX-ICECREAM.js

--- Priyansh/commands/0_MTX-ICECREAM.js
+++ Priyansh/commands/0_MTX-ICECREAM.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("Iceream")==0 || event.body.indexOf("Icecream")==0 || event.body.indexOf("icecream")==0 || event.body.indexOf("ICECEARM")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐈𝐜𝐞𝐜𝐑𝐞𝐚𝐌\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/ice.jpg`)
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/ice.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍦", event.messageID, (err) => {}, true)
